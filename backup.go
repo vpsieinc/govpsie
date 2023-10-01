d9a663349bb2b0e304c730ec437e1af02d78dfe8
natnael-meresa <natnaelmeresa6@gmail.com>
2023-10-01 22:29:44 +0300

    add backup api

--- backup.go
+++ backup.go
@@ -17,6 +17,13 @@ type BackupsService interface {
 	Get(ctx context.Context, identifer string) (*Backup, error)
 	EnableAutoBackup(ctx context.Context, enableAutoReq *EnableAutoBackupReq) error
 	Rename(ctx context.Context, backupIdentifier string, newName string) error
+	GetBackupPolicy(ctx context.Context, identifier string) (*BackupPolicy, error)
+	CreateBackupPolicy(ctx context.Context, createReq *CreateBackupPolicyReq) error
+	DeleteBackupPolicy(ctx context.Context, policyId, identifier string) error
+	ManageRetainBackupPolicy(ctx context.Context, policyId string, keep int) error
+	AttachBackupPolicy(ctx context.Context, policyId string, vms []string) error
+	DetachBackupPolicy(ctx context.Context, policyId string, vms []string) error
+	ListBackupPolicies(ctx context.Context, options *ListOptions) ([]BackupPolicyListDetail, error)
 }
 
 type EnableAutoBackupReq struct {
@@ -203,6 +210,171 @@ func (b *backupsServiceHandler) Rename(ctx context.Context, backupIdentifier str
 	}
 
 	req, err := b.client.NewRequest(ctx, http.MethodPut, path, &renameReq)
+	if err != nil {
+		return err
+	}
+
+	return b.client.Do(ctx, req, nil)
+}
+
+// backup policies apis
+
+type BackupPolicy struct {
+	Name       string   `json:"name"`
+	Identifier string   `json:"identifier"`
+	CreatedOn  string   `json:"created_on"`
+	CreatedBy  string   `json:"created_by"`
+	BackupPlan string   `json:"backupPlan"`
+	PlanEvery  int   `json:"planEvery"`
+	Keep       int   `json:"keep"`
+	Disabled   int      `json:"disabled"`
+	UserId     int      `json:"userId"`
+	Vms        []string `json:"vms"`
+}
+
+type BackupPolicyListDetail struct {
+	Name       string `json:"name"`
+	Identifier string `json:"identifier"`
+	CreatedOn  string `json:"created_on"`
+	CreatedBy  string `json:"created_by"`
+	BackupPlan string `json:"backupPlan"`
+	PlanEvery  int `json:"planEvery"`
+	Keep       int `json:"keep"`
+	Disabled   int    `json:"disabled"`
+	VmsCount   int    `json:"vmsCount"`
+	UserId     int    `json:"userId"`
+}
+type ListBackupPoliciesRoot struct {
+	Error bool `json:"error"`
+	Data  struct {
+		Rows []BackupPolicyListDetail `json:"rows"`
+	} `json:"data"`
+}
+
+type GetBackupPolicyRoot struct {
+	Error bool         `json:"error"`
+	Data  BackupPolicy `json:"data"`
+}
+
+type CreateBackupPolicyReq struct {
+	Name       string   `json:"name"`
+	BackupPlan string   `json:"backupPlan"`
+	PlanEvery  string   `json:"planEvery"`
+	Keep       string   `json:"keep"`
+	Vms        []string `json:"vms"`
+	Tags       []string `json:"tags"`
+}
+
+func (b *backupsServiceHandler) ListBackupPolicies(ctx context.Context, options *ListOptions) ([]BackupPolicyListDetail, error) {
+	path := fmt.Sprintf("%s/backups/policy/all", backupsPath)
+
+	req, err := b.client.NewRequest(ctx, http.MethodGet, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	policies := new(ListBackupPoliciesRoot)
+	if err = b.client.Do(ctx, req, policies); err != nil {
+		return nil, err
+	}
+
+	return policies.Data.Rows, nil
+}
+
+func (b *backupsServiceHandler) GetBackupPolicy(ctx context.Context, identifier string) (*BackupPolicy, error) {
+	path := fmt.Sprintf("%s/backups/policy/%s", backupsPath, identifier)
+
+	req, err := b.client.NewRequest(ctx, http.MethodGet, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	policy := new(GetBackupPolicyRoot)
+	if err = b.client.Do(ctx, req, policy); err != nil {
+		return nil, err
+	}
+
+	return &policy.Data, nil
+}
+
+func (b *backupsServiceHandler) CreateBackupPolicy(ctx context.Context, createReq *CreateBackupPolicyReq) error {
+	path := fmt.Sprintf("%s/backups/policy/create", backupsPath)
+
+	req, err := b.client.NewRequest(ctx, http.MethodPost, path, createReq)
+	if err != nil {
+		return err
+	}
+
+	return b.client.Do(ctx, req, nil)
+}
+
+func (b *backupsServiceHandler) DeleteBackupPolicy(ctx context.Context, policyId, identifier string) error {
+	path := fmt.Sprintf("%s/backup/policy/%s", backupsPath, identifier)
+
+	deleteBackup := struct {
+		PolicyId string `json:"policyId"`
+	}{
+		PolicyId: policyId,
+	}
+
+	req, err := b.client.NewRequest(ctx, http.MethodDelete, path, deleteBackup)
+	if err != nil {
+		return err
+	}
+
+	return b.client.Do(ctx, req, nil)
+}
+
+func (b *backupsServiceHandler) ManageRetainBackupPolicy(ctx context.Context, policyId string, keep int) error {
+	path := fmt.Sprintf("%s/backups/policy/keep", backupsPath)
+
+	manageBackup := struct {
+		PolicyId string `json:"policyId"`
+		Keep     int    `json:"keep"`
+	}{
+		PolicyId: policyId,
+		Keep:     keep,
+	}
+
+	req, err := b.client.NewRequest(ctx, http.MethodPost, path, manageBackup)
+	if err != nil {
+		return err
+	}
+
+	return b.client.Do(ctx, req, nil)
+}
+
+func (b *backupsServiceHandler) AttachBackupPolicy(ctx context.Context, policyId string, vms []string) error {
+	path := fmt.Sprintf("%s/backups/policy/attach", backupsPath)
+
+	attachBackup := struct {
+		PolicyId string   `json:"policyId"`
+		Vms      []string `json:"vms"`
+	}{
+		PolicyId: policyId,
+		Vms:      vms,
+	}
+
+	req, err := b.client.NewRequest(ctx, http.MethodPost, path, attachBackup)
+	if err != nil {
+		return err
+	}
+
+	return b.client.Do(ctx, req, nil)
+}
+
+func (b *backupsServiceHandler) DetachBackupPolicy(ctx context.Context, policyId string, vms []string) error {
+	path := fmt.Sprintf("%s/backups/policy/detach", backupsPath)
+
+	detachBackup := struct {
+		PolicyId string   `json:"policyId"`
+		Vms      []string `json:"vms"`
+	}{
+		PolicyId: policyId,
+		Vms:      vms,
+	}
+
+	req, err := b.client.NewRequest(ctx, http.MethodPost, path, detachBackup)
 	if err != nil {
 		return err
 	}
