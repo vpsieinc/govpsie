0299d1e31ad0000de99c0118ce09013f453d1759
natnael-meresa <natnaelmeresa6@gmail.com>
2023-11-25 09:44:06 +0300

    add attacha and detach funcations

--- firewallGroup.go
+++ firewallGroup.go
@@ -4,19 +4,22 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 var firewallGroupBasePath = "/apps/v2/firewall"
 
 type FirewallGroupService interface {
-	Create(ctx context.Context, groupName string) error
-	List(ctx context.Context, options *ListOptions) ([]FirewallGroup, error)
-	Get(ctx context.Context, fwGroupId string) (*FirewallGroup, error)
+	Create(ctx context.Context, groupName string, firewallUpdateReq []FirewallUpdateReq) error
+	List(ctx context.Context, options *ListOptions) ([]FirewallGroupListData, error)
+	Get(ctx context.Context, fwGroupId string) (*FirewallGroupDetailData, error)
 	Delete(ctx context.Context, fwGroupId string) error
 	Update(ctx context.Context, fwGroupReq *FirewallUpdateReq, fwGroupId string) error
-	AssignToVpsie(ctx context.Context, groupId string, vmId string) error
+	AssignToVpsie(ctx context.Context, groupId, vmId string) error
+	DetachFromVpsie(ctx context.Context, groupId, vmId string) error
+	AttachToVpsie(ctx context.Context, groupId, vmId string) error
 	DeleteFirewallGroupOfServer(ctx context.Context, groupId, vmId string) error
-	GetFirewallGroup(ctx context.Context, fwGroupId string) (*FirewallGroup, error)
+	GetFirewallGroup(ctx context.Context, fwGroupId string) (*FirewallGroupDetailData, error)
 }
 
 type firewallGroupServiceHandler struct {
@@ -26,16 +29,90 @@ type firewallGroupServiceHandler struct {
 var _ FirewallGroupService = &firewallGroupServiceHandler{}
 
 type ListFirewallGroupsRoot struct {
-	Error bool            `json:"error"`
-	Data  []FirewallGroup `json:"data"`
-	Total int             `json:"total"`
+	Error bool                    `json:"error"`
+	Data  []FirewallGroupListData `json:"data"`
+	Total int                     `json:"total"`
 }
 
 type GetFirewallGroupRoot struct {
-	Error bool `json:"error"`
-	Data  struct {
-		Group *FirewallGroup `json:"group"`
-	} `json:"data"`
+	Error bool                    `json:"error"`
+	Data  FirewallGroupDetailData `json:"data"`
+}
+
+type FirewallGroupListData struct {
+	UserName      string `json:"user_name"`
+	ID            int    `json:"id"`
+	GroupName     string `json:"group_name"`
+	Identifier    string `json:"identifier"`
+	CreatedOn     string `json:"created_on"`
+	UpdatedOn     string `json:"updated_on"`
+	InboundCount  int    `json:"inbound_count"`
+	OutboundCount int    `json:"outbound_count"`
+	Vms           int    `json:"vms"`
+	CreatedBy     int    `json:"created_by"`
+
+	Rules   []FirewallRules `json:"rules"`
+	VmsData []VmsData       `json:"vmsData"`
+}
+type FirewallGroupDetailData struct {
+	Group FirewallGroup   `json:"group"`
+	Rules []FirewallRules `json:"rules"`
+	Vms   []VmsData       `json:"vms"`
+	Count int64           `json:"count"`
+}
+
+type VmsData struct {
+	Hostname   string `json:"hostname"`
+	Identifier string `json:"identifier"`
+	Fullname   string `json:"fullname"`
+	Category   string `json:"category"`
+}
+
+type FirewallRules struct {
+	InBound  []InBoundFirewallRules  `json:"inBound"`
+	OutBound []OutBoundFirewallRules `json:"outBound"`
+}
+
+type OutBoundFirewallRules struct {
+	ID         int       `json:"id"`
+	GroupID    int       `json:"group_id"`
+	UserID     int       `json:"user_id"`
+	Action     string    `json:"action"`
+	Type       string    `json:"type"`
+	Comment    string    `json:"comment"`
+	Dest       []string  `json:"dest,omitempty"`
+	Dport      string    `json:"dport"`
+	Proto      string    `json:"proto"`
+	Source     []string  `json:"source"`
+	Sport      string    `json:"sport"`
+	Enable     int       `json:"enable"`
+	Iface      string    `json:"iface,omitempty"`
+	Log        string    `json:"log,omitempty"`
+	Macro      string    `json:"macro,omitempty"`
+	Identifier string    `json:"identifier"`
+	CreatedOn  time.Time `json:"created_on"`
+	UpdatedOn  time.Time `json:"updated_on"`
+}
+
+type InBoundFirewallRules struct {
+	ID         int       `json:"id"`
+	GroupID    int       `json:"group_id"`
+	UserID     int       `json:"user_id"`
+	Action     string    `json:"action"`
+	Type       string    `json:"type"`
+	Comment    string    `json:"comment"`
+	Dest       []string  `json:"dest"`
+	Dport      string    `json:"dport"`
+	Proto      string    `json:"proto"`
+	Source     []string  `json:"source,omitempty"`
+	Sport      string    `json:"sport"`
+	Enable     int       `json:"enable"`
+	Iface      string    `json:"iface,omitempty"`
+	Log        string    `json:"log,omitempty"`
+	Macro      string    `json:"macro,omitempty"`
+	Identifier string    `json:"identifier"`
+	CreatedOn  time.Time `json:"created_on"`
+	UpdatedOn  time.Time `json:"updated_on"`
 }
 
 type FirewallGroup struct {
@@ -48,35 +125,33 @@ type FirewallGroup struct {
 	InboundCount  int    `json:"inbound_count"`
 	OutboundCount int    `json:"outbound_count"`
 	Vms           int    `json:"vms"`
+	CreatedBy     int    `json:"created_by"`
 }
 
 type FirewallUpdateReq struct {
-	Action     string     `json:"action"`
-	Type       string     `json:"type"`
-	Comment    string     `json:"comment"`
-	Dest       []IpsetObj `json:"dest"`
-	Dport      string     `json:"dport"`
-	Source     []IpsetObj `json:"source"`
-	Proto      string     `json:"proto"`
-	Sport      string     `json:"sport"`
-	Enable     int        `json:"enable"`
-	Iface      string     `json:"iface"`
-	Log        string     `json:"log"`
-	Macro      string     `json:"macro"`
-	Identifier string     `json:"identifier"`
+	Action  string   `json:"action"`
+	Type    string   `json:"type"`
+	Dport   string   `json:"dport"`
+	Proto   string   `json:"proto"`
+	Source  []string `json:"source,omitempty"`
+	Sport   string   `json:"sport"`
+	Enable  int      `json:"enable"`
+	Macro   string   `json:"macro"`
+	Comment string   `json:"comment"`
+	Dest    []string `json:"dest,omitempty"`
 }
 
 type IpsetObj struct {
 	Ipset string `json:"ipset"`
 }
 
-var _ FirewallGroupService = &firewallGroupServiceHandler{}
-
-func (f *firewallGroupServiceHandler) Create(ctx context.Context, groupName string) error {
+func (f *firewallGroupServiceHandler) Create(ctx context.Context, groupName string, firewallUpdateReq []FirewallUpdateReq) error {
 	fwGroupReq := struct {
-		GroupName string `json:"groupName"`
+		GroupName string              `json:"groupName"`
+		Rules     []FirewallUpdateReq `json:"rules,omitempty"`
 	}{
 		GroupName: groupName,
+		Rules:     firewallUpdateReq,
 	}
 
 	path := fmt.Sprintf("%s/create/group", firewallGroupBasePath)
@@ -89,7 +164,7 @@ func (f *firewallGroupServiceHandler) Create(ctx context.Context, groupName stri
 	return f.client.Do(ctx, req, nil)
 }
 
-func (f *firewallGroupServiceHandler) List(ctx context.Context, options *ListOptions) ([]FirewallGroup, error) {
+func (f *firewallGroupServiceHandler) List(ctx context.Context, options *ListOptions) ([]FirewallGroupListData, error) {
 	path := fmt.Sprintf("%s/groups", firewallGroupBasePath)
 
 	req, err := f.client.NewRequest(ctx, http.MethodGet, path, nil)
@@ -107,7 +182,7 @@ func (f *firewallGroupServiceHandler) List(ctx context.Context, options *ListOpt
 
 }
 
-func (f *firewallGroupServiceHandler) Get(ctx context.Context, fwGroupId string) (*FirewallGroup, error) {
+func (f *firewallGroupServiceHandler) Get(ctx context.Context, fwGroupId string) (*FirewallGroupDetailData, error) {
 	path := fmt.Sprintf("%s/group/%s", firewallGroupBasePath, fwGroupId)
 
 	req, err := f.client.NewRequest(ctx, http.MethodGet, path, nil)
@@ -121,7 +196,7 @@ func (f *firewallGroupServiceHandler) Get(ctx context.Context, fwGroupId string)
 		return nil, err
 	}
 
-	return fwGroup.Data.Group, nil
+	return &fwGroup.Data, nil
 }
 
 func (f *firewallGroupServiceHandler) Delete(ctx context.Context, fwGroupId string) error {
@@ -160,6 +235,44 @@ func (f *firewallGroupServiceHandler) AssignToVpsie(ctx context.Context, groupId
 	return f.client.Do(ctx, req, nil)
 }
 
+func (f *firewallGroupServiceHandler) AttachToVpsie(ctx context.Context, groupId, vmId string) error {
+	path := fmt.Sprintf("%s/attach/group", firewallGroupBasePath)
+
+	assignReq := struct {
+		VmID    string `json:"vmId"`
+		GroupID string `json:"groupId"`
+	}{
+		VmID:    vmId,
+		GroupID: groupId,
+	}
+
+	req, err := f.client.NewRequest(ctx, http.MethodPost, path, assignReq)
+	if err != nil {
+		return err
+	}
+
+	return f.client.Do(ctx, req, nil)
+}
+
+func (f *firewallGroupServiceHandler) DetachFromVpsie(ctx context.Context, groupId, vmId string) error {
+	path := fmt.Sprintf("%s/detach/group", firewallGroupBasePath)
+
+	assignReq := struct {
+		VmID    string `json:"vmId"`
+		GroupID string `json:"groupId"`
+	}{
+		VmID:    vmId,
+		GroupID: groupId,
+	}
+
+	req, err := f.client.NewRequest(ctx, http.MethodPost, path, assignReq)
+	if err != nil {
+		return err
+	}
+
+	return f.client.Do(ctx, req, nil)
+}
+
 func (f *firewallGroupServiceHandler) Update(ctx context.Context, fwGroupReq *FirewallUpdateReq, fwGroupId string) error {
 	path := fmt.Sprintf("%s/groups/%s", firewallGroupBasePath, fwGroupId)
 
@@ -171,7 +284,7 @@ func (f *firewallGroupServiceHandler) Update(ctx context.Context, fwGroupReq *Fi
 	return f.client.Do(ctx, req, nil)
 }
 
-func (f *firewallGroupServiceHandler) GetFirewallGroup(ctx context.Context, fwGroupId string) (*FirewallGroup, error) {
+func (f *firewallGroupServiceHandler) GetFirewallGroup(ctx context.Context, fwGroupId string) (*FirewallGroupDetailData, error) {
 	path := fmt.Sprintf("%s/group/%s", firewallGroupBasePath, fwGroupId)
 
 	req, err := f.client.NewRequest(ctx, http.MethodGet, path, nil)
@@ -185,7 +298,7 @@ func (f *firewallGroupServiceHandler) GetFirewallGroup(ctx context.Context, fwGr
 		return nil, err
 	}
 
-	return fwGroup.Data.Group, nil
+	return &fwGroup.Data, nil
 }
 
 func (f *firewallGroupServiceHandler) DeleteFirewallGroupOfServer(ctx context.Context, groupId, vmId string) error {
